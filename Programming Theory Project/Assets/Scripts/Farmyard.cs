2426e30de9ee8c60b2c4f24ccf368276216a500e
Brian McKeon <brian.work.address@gmail.com>
2022-11-09 16:58:55 -0500

    Worked on debugging and new resource types.

--- Programming Theory Project/Assets/Scripts/Farmyard.cs
+++ Programming Theory Project/Assets/Scripts/Farmyard.cs
@@ -7,6 +7,12 @@ public class Farmyard : MonoBehaviour
     public GameObject resourcePrefab;
     private GameObject ground;
 
+    // The number of resource tile rows and columns
+    private int rows, columns;
+
+    // The 2D array of resource tiles
+    private List<List<GameObject>> resources = new List<List<GameObject>>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,29 +32,58 @@ public class Farmyard : MonoBehaviour
         var resourceMesh = resourcePrefab.GetComponent<MeshRenderer>();
 
         // Determine how many rows and columns of resource prefabs we can fit within our bounds
-        var rows = groundMesh.bounds.extents.z / resourceMesh.bounds.extents.z;
-        var columns = groundMesh.bounds.extents.x / resourceMesh.bounds.extents.x;
+        rows = (int)(groundMesh.bounds.extents.z / resourceMesh.bounds.extents.z);
+        columns = (int)(groundMesh.bounds.extents.x / resourceMesh.bounds.extents.x);
 
         //Debug.Log($"Ground: z: {groundMesh.bounds.extents.z} x: {groundMesh.bounds.extents.x}");
         Debug.Log($"Resource: z: {resourceMesh.bounds.extents.z} x: {resourceMesh.bounds.extents.x}");
-
-        // x -25 25
-        // z -15 35
         Debug.Log($"Ground {groundMesh.bounds.min.z} {groundMesh.bounds.max.z}");
 
         float startX = groundMesh.bounds.min.x + resourceMesh.bounds.extents.x;
         float startZ = groundMesh.bounds.min.z + resourceMesh.bounds.extents.z;
 
+        // Loop over rows and columns creating resource tiles and store them for later in our resources array.
         for (int r = 0; r < rows; r++)
         {
+            List<GameObject> rowList = new List<GameObject>();
+
             for (int c = 0; c < columns; c++)
             {
+                // Calculate new position for this resource and instantiate a prefab
                 var pos = new Vector3(
                     startX + c * resourceMesh.bounds.extents.x * 2,
                     0,
                     startZ + r * resourceMesh.bounds.extents.z * 2);
-                Instantiate(resourcePrefab, pos, resourcePrefab.transform.rotation);
+                GameObject tile = Instantiate(resourcePrefab, pos, resourcePrefab.transform.rotation);
+
+                rowList.Add(tile);
             }
+
+            resources.Add(rowList);
         }
+
+        debugResourcesArray();
+    }
+
+    private void debugResourcesArray()
+    {
+        Debug.Log("Resources Array:");
+
+        int totalTiles = 0;
+
+        for (int r = 0; r < resources.Count; r++)
+        {
+            var rowList = resources[r];
+            totalTiles += rowList.Count;
+
+            string rowString = "";
+            for (int c = 0; c < rowList.Count; c++)
+            {
+                rowString += $"[{c}, {r}: {rowList[c].ToString()}]    ";
+            }
+            Debug.Log(rowString);
+        }
+
+        Debug.Log($"Total: {totalTiles}");
     }
 }
---
+++ Programming Theory Project/Assets/Scripts/Grass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Grass resource
+public class Grass : Resource
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
